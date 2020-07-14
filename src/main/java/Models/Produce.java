19752d8117e04bcc4bcec9180c659403d814f46c
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-14 15:26:31 +0100

    table complete

--- src/main/java/Models/Produce.java
+++ src/main/java/Models/Produce.java
@@ -1,14 +1,39 @@
 package Models;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="Produce")
 public class Produce {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="product_type")
     private String type;
+
+    @Column(name="product_style")
     private String style;
+
+    @Column(name="product_name")
     private String name;
+
+    @Column(name="place_of_origin")
     private String origin;
+
+    @Column(name="product_info")
     private String info;
+
+    @Column(name="image")
     private String imgLink;
+
+    @Column(name="retail_price")
     private Double costPrice;
+
+    @Column(name="retail_price")
     private Double retailPrice;
 
     public Produce(String type, String style, String name,String origin, String info, String imgLink, Double costPrice, Double retailPrice) {

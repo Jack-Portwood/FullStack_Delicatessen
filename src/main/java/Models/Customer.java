d1e834119e52042b9ffb81d10262787d9f580cff
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-14 11:03:09 +0100

    initial models set up

--- src/main/java/Models/Customer.java
+++ src/main/java/Models/Customer.java
@@ -1,4 +1,52 @@
 package Models;
 
+import java.util.ArrayList;
+
 public class Customer {
+
+    private Long id;
+    private String firstName;
+    private String secondName;
+    private ArrayList<Order>orders;
+
+    public Customer(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.orders = new ArrayList<>();
+    }
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
 }
--- src/main/java/Models/Produce.java
+++ src/main/java/Models/Produce.java
@@ -1,4 +1,91 @@
 package Models;
 
 public class Produce {
+    private Long id;
+    private String type;
+    private String style;
+    private String name;
+    private String info;
+    private String imgLink;
+    private Double costPrice;
+    private Double retailPrice;
+
+    public Produce(String type, String style, String name, String info, String imgLink, Double costPrice, Double retailPrice) {
+        this.type = type;
+        this.style = style;
+        this.name = name;
+        this.info = info;
+        this.imgLink = imgLink;
+        this.costPrice = costPrice;
+        this.retailPrice = retailPrice;
+    }
+
+    public Produce() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getImgLink() {
+        return imgLink;
+    }
+
+    public void setImgLink(String imgLink) {
+        this.imgLink = imgLink;
+    }
+
+    public Double getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(Double costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public Double getRetailPrice() {
+        return retailPrice;
+    }
+
+    public void setRetailPrice(Double retailPrice) {
+        this.retailPrice = retailPrice;
+    }
 }
+
+

16e9c3df19ac77a9812929e3b036065e5216c835
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-15 10:08:57 +0100

    get routes complete in order class

--- src/main/java/Controllers/OrderController.java
+++ src/main/java/Controllers/OrderController.java
@@ -1,4 +1,31 @@
 package Controllers;
 
+import Models.Order;
+import Repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class OrderController {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @GetMapping(value = "/orders")
+    public ResponseEntity<List<Order>> getAllOrders(){
+        return new ResponseEntity<>(orderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value ="/orders/{id}")
+    public ResponseEntity getOrder(@PathVariable Long id){
+        return new ResponseEntity<>(orderRepository.findById(id), HttpStatus.OK);
+    }
+
+
 }

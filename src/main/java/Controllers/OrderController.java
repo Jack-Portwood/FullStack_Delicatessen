1c573ed9748a273d786622c5b52f29f5e9e241e7
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-15 10:20:46 +0100

    all routes complete for order

--- src/main/java/Controllers/OrderController.java
+++ src/main/java/Controllers/OrderController.java
@@ -5,9 +5,7 @@ import Repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,23 @@ public class OrderController {
         return new ResponseEntity<>(orderRepository.findById(id), HttpStatus.OK);
     }
 
+    @PostMapping(value = "/orders")
+    public ResponseEntity<Order> postOrder(@RequestBody Order order){
+        orderRepository.save(order);
+        return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+    @PatchMapping(value= "/orders.{id}")
+    public ResponseEntity<Order>updateOrder(@RequestBody Order order){
+        orderRepository.save(order);
+        return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/orders/{id}")
+    public ResponseEntity<Order> deleteOrder(@PathVariable Long id){
+        Order found = orderRepository.getOne(id);
+        orderRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 
 }

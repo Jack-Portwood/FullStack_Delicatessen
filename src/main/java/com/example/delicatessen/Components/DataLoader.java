5ed1f8ae05922030e4bbf6d83c5cd73be06bbe87
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-15 14:54:55 +0100

    Dataloader complete

--- src/main/java/com/example/delicatessen/Components/DataLoader.java
+++ src/main/java/com/example/delicatessen/Components/DataLoader.java
@@ -1,6 +1,7 @@
 package com.example.delicatessen.Components;
 
 import com.example.delicatessen.Models.Customer;
+import com.example.delicatessen.Models.Order;
 import com.example.delicatessen.Models.Produce;
 import com.example.delicatessen.Repositories.CustomerRepository;
 import com.example.delicatessen.Repositories.OrderRepository;
@@ -30,6 +31,9 @@ public class DataLoader implements ApplicationRunner {
 
         Customer customer1 = new Customer("Jack", "Portwood");
         customerRepository.save(customer1);
+
+        Customer customer2 = new Customer("Abby", "Cooper");
+        customerRepository.save(customer2);
 
         Produce produce1 = new Produce("Blue Cheese","Stilton","Hebridean Blue","Scotland", "Scotland\'s take on a classic Stilton. Aged for 8 months this blue cheese made by the Reid family is a wonderfully subtle and creamy blue. Salty and tangy with a creamy texture.", "https://mellischeese.net/wp-content/uploads/2017/10/IMG_9671.jpg", 10.00, 7.00  );
         produceRepository.save(produce1);
@@ -64,8 +68,33 @@ public class DataLoader implements ApplicationRunner {
         Produce produce11 = new Produce("Soft Cheese","Camembert", "Camembert Reaux", "France", "The classic French Camembert from one of the very best producers. It can be pungent and vegetal with mushroom-y undertones, though when slightly younger it is lactic and milky. Camembert Reaux is creamy rich texture and melting mouth feel.", "https://mellischeese.net/wp-content/uploads/2017/10/IMG_9687.jpg", 4.00, 8.50);
         produceRepository.save(produce11);
 
-        Produce produce12 = new Produce("Soft Cheese", "Ewe\'s Milk Cheese", "Lancelot", "Scotland","A fresh ewe\'s milk cheese made in Lanarkshire. Lemony, citrusy and fresh flavours with a creamy texture. Made in Lanarkshire, Scotland.","https://mellischeese.net/wp-content/uploads/2020/04/lancelot.jpg", 4.00, 9.00 );
+        Produce produce12 = new Produce("Soft Cheese", "Ewe\'s Milk Cheese", "Lancelot", "Scotland","A fresh ewe\'s milk cheese made in Lanarkshire. Lemony, citrusy and fresh flavours with a creamy texture. Made in Lanarkshire, Scotland.","https://mellischeese.net/wp-content/uploads/2020/04/lancelot.jpg", 4.00, 9.00);
         produceRepository.save(produce12);
+
+        Produce produce13 = new Produce("Biscuit", "Oatcake", "Adamsons Oatcakes", "Scotland", "A classic Scottish Oatcake made in Pittenweem, Fife. Crumbly, earthy and oaty. A perfect addition to a cheeseboard and fantastic with our Spicy Tomato Chutney", "https://mellischeese.net/wp-content/uploads/2017/10/IMG_0059.jpg", 00.50, 2.25);
+        produceRepository.save(produce13);
+
+        Produce produce14 = new Produce("Flour", "Pasta Flour", "Tre Grazie Tipo '00'", "Italy", "Tre Grazie means ‘the three graces’, and each bag of flour is adorned with an illustration of beauty, mirth and elegance", "https://mellischeese.net/wp-content/uploads/2020/04/pasta-flour-copy.jpg", 00.50, 2.96);
+        produceRepository.save(produce14);
+
+        Produce produce15 = new Produce("Flour", "Beard Flour", "Mungoswells Strong Bread Flour", "Scotland", "Strong Bread flour made by Mungoswells in East Lothian, Scotland. Their flour is made entirely from wheat grown in East Lothian. They grow different varieties of wheat suitable for making different types of flour.", "https://mellischeese.net/wp-content/uploads/2020/04/bread-flour-1-1.jpg", 1.00, 3.10);
+        produceRepository.save(produce15);
+
+        Produce produce16 = new Produce("Preserve", "Fruit", "Rosebud Fig Chutney", "Scotland", "Luxuary Chutney compliments any cheese board.", "https://mellischeese.net/wp-content/uploads/2017/10/IMG_0113.jpg", 1.50, 5.30);
+        produceRepository.save(produce16);
+
+
+//        Order order1 = new Order(customer1);
+//        orderRepository.save(order1);
+//        order1.addProduce(produce7);
+//        order1.addProduce(produce10);
+//        order1.addProduce(produce13);
+//
+//        orderRepository.save(order1);
+
+
+
+
 
 
     }

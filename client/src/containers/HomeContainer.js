dfc0f2ce2a4f5894cf3a9d47cd79783f5357870e
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-17 14:15:13 +0100

    images resized

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -15,30 +15,28 @@ const inlineStyle = {
 }
 function HomeContainer() {
   return (
-    <div style={{textAlign: 'center'}}>
-      
-        <Parallax bgImage={image1} strength={500}>
-          <div style={{ height: 500 }}>
-            <div style={inlineStyle}>Info box</div>
-          </div>
-        </Parallax>
+    <div style={{ textAlign: "center" }}>
+      <Parallax bgImage={image1} strength={500} blur={{ min: -1, max: 3 }}>
+        <div style={{ height: 800 }}>
+          <div style={inlineStyle}>Our Story</div>
+        </div>
+      </Parallax>
 
-        <h1>. . .</h1>
-        <Parallax bgImage={image2} blur={{ min: -1, max: 6 }}>
-          <div style={{ height: 500 }}>
-            <div style={inlineStyle}>Blur effect</div>
-          </div>
-        </Parallax>
+      <h1>. . .</h1>
+      <Parallax bgImage={image2} blur={{ min: -1, max: 6 }}>
+        <div style={{ height: 800 }}>
+          <p style={inlineStyle}>Our Vision</p>
+        </div>
+      </Parallax>
 
-        <h1>. . .</h1>
-        <Parallax bgImage={image3} strength={-200}>
-          <div style={{ height: 500 }}>
-            <div style={inlineStyle}>reverse direction</div>
-          </div>
-        </Parallax>
-        <div style={{ height: "100vh" }}></div>
-      </div>
-    
+      <h1>. . .</h1>
+      <Parallax bgImage={image3} strength={-200}>
+        <div style={{ height: 800 }}>
+          <div style={inlineStyle}>Our Promise</div>
+        </div>
+      </Parallax>
+      <div style={{ height: "50vh" }}></div>
+    </div>
   );
 }
 export default HomeContainer;

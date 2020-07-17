d163886a2e8d1b339189fb9c59c79775918c7d16
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-17 10:33:04 +0100

    requests set up

---
+++ client/src/helpers/requests.js
@@ -0,0 +1,23 @@
+class Request {
+
+    get(url){
+        return fetch(url)
+        .then((res) => res.json);
+    }
+
+    delete(url){
+        return fetch(url, {
+            method: "Delete",
+            headers:{"Content-Type": "application/json"}
+        })
+    }
+
+    post(url, playload){
+        return fetch(url, {
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(payload)
+        });
+    }
+}
+
+export default Request;

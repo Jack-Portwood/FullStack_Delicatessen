1fef4b4ceb4cc0eaf0488bda83b31fa7cd135155
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-20 13:20:34 +0100

    project renders need pull data through

--- client/src/containers/ProduceContainer.js
+++ client/src/containers/ProduceContainer.js
@@ -1,6 +1,9 @@
 import React, {Fragment} from "react";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import BlueCheeseComponent from "../components/BlueCheeseComponent";
+import BlueCheeseComponent from "../components/BlueCheeseComponent.js";
+import HardCheeseComponent from "../components/HardCheeseComponent.js";
+import SoftCheeseComponent from "../components/SoftCheeseComponent.js";
+import PantryComponent from "../components/PantryComponent.js";
 import { render } from "@testing-library/react";
 
 
@@ -8,18 +11,45 @@ import { render } from "@testing-library/react";
 function ProduceContainer(){
 
     render()
-        return(
-            <Router>
-                <Fragment>
-                <Switch>
-                    <Route render = {(props) => {
-                        return <BlueCheeseComponent BlueCheeseComponent={this.state.BlueCheeseComponent}/>
-                    }}/>
-                </Switch>
-                </Fragment>
-            </Router>
+        return (
+          <Router>
+            <Fragment>
+              <Switch>
+                <Route
+                  exact
+                  path="/produce/bluecheese"
+                  render={() => {
+                    return <BlueCheeseComponent onCreate={this.handlePost} />;
+                  }}
+                />
 
-        )
+                <Route
+                  exact
+                  path="/produce/hardcheese"
+                  render={() => {
+                    return <HardCheeseComponent onCreate={this.handlePost} />;
+                  }}
+                />
+
+                <Route
+                  exact
+                  path="/produce/softcheese"
+                  render={() => {
+                    return <SoftCheeseComponent onCreate={this.handlePost} />;
+                  }}
+                />
+
+                <Route
+                  exact
+                  path="/produce/pantry"
+                  render={() => {
+                    return <PantryComponent onCreate={this.handlePost} />;
+                  }}
+                />
+              </Switch>
+            </Fragment>
+          </Router>
+        );
 
     }
 

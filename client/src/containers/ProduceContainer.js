47936b5acf5ca26e78691b5958a4a420308179c3
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-21 12:01:21 +0100

    code cleaned up

--- client/src/containers/ProduceContainer.js
+++ client/src/containers/ProduceContainer.js
@@ -1,45 +1,10 @@
-import React, {Fragment, Component} from "react";
-import {BrowserRouter as Router, Route, Switch, NavLink,Link} from 'react-router-dom';
-import BlueCheeseComponent from "../components/BlueCheese/BlueCheeseComponent.js";
-import HardCheeseComponent from "../components/HardCheese/HardCheeseComponent.js";
-import SoftCheeseComponent from "../components/SoftCheese/SoftCheeseComponent.js";
-import PantryComponent from "../components/Pantry/PantryComponent.js";
-import Requests from "../helpers/requests";
+import React from "react";
+import {Link} from 'react-router-dom';
+;
 
 
 const ProduceContainer = () => {
-    // constructor(props){
-    //     super(props)
-    //     this.state = {
-    //         produce: []
-    //     }
-    // }
-
-    // componentDidMount(){
-    //     const requests = new Requests();
-    //     requests.get('/api/produce')
-    //     .then((data) => {
-    //         this.setState({produce: data})
-    //     })
-    // }
-
-    // handlePost(produce){
-    //     const requests = new Requests();
-    //     requests.post('/api/produce', produce)
-    //     .then ( () => {
-    //         window.location = '/pirates'
-    //     })
-    // }
-
-
-  
-
     
-    //     render(){
-
-    //     if(!this.state.produce){
-    //         return null
-    //     }
     return (
       <div>
         <ul>

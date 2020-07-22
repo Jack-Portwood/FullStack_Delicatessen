4584a67cb801e2b925589cc42012e90fb7250805
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-22 13:46:42 +0100

    commit

--- client/src/components/ProduceComponent.js
+++ client/src/components/ProduceComponent.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Request from "../helpers/request.js";
+import ProduceDetail from "./ProduceDetail"
 
 function ProduceComponent(props) {
   const [produce, setProduce] = useState([]);
@@ -11,18 +12,18 @@ function ProduceComponent(props) {
     request.get(url).then((data) => setProduce(data));
   }, []);
 
+  //maps over array and outputs indiviual items
   const stock = produce.map((item, index) => (
     <div>
       {item.name}
       <img className="product-image" src={item.imgLink} />
-
     </div>
   ));
 
   return (
-  <div className = "produce-container"> 
-  {stock} 
-  </div>
+      <div className = "produce-container"> 
+        {stock} 
+      </div>
   );
 }
 
---
+++ client/src/components/ProduceDetail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ProduceComponent from "./ProduceComponent"
+import { render } from '@testing-library/react';
+
+function ProduceDetail (props) {
+
+    // render(){
+    //     if(!props.product){
+    //         return "loading..."
+    //     }
+    
+
+    
+        return (
+            <p>
+                Info: 
+            </p>
+        )
+    }
+
+
+export default ProduceDetail;

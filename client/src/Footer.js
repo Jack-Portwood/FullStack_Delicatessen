b8c4590410af751f82fc7ea62f4eaea7e5a6d2a9
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-31 12:47:29 +0100

    footer refactored

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,18 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {NavLink} from "react-router-dom"
 
 const Footer = (props) => {
     return (
       <footer className="footer"> 
-      <div>
         <div className="address">
-        <p>432 Ockham Road East Thirston NE65 3NB  078 8209 8789</p>
+        432<br/> Ockham Road<br/> East Thirston<br/> NE65 3NB <br/> 078 8209 8789
         </div>
+
+        <NavLink to="/contact"className="footlink1"> Contact </NavLink>
         
-        <NavLink to="/contact"className="navlink"> Contact </NavLink>
-        
-        <NavLink to="/basket" className="navlink"> Basket  </NavLink>
-      </div>
+        <NavLink to="/basket" className="footlink2"> Basket  </NavLink>
+      
       </footer>
     );
 }

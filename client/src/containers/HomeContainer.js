a29036d4689f1f24b4ac3e3b31931e27b2b92817
Jack Portwood <Jack.Portwood14@gmail.com>
2020-07-31 14:26:49 +0100

    text in place needs styled

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -20,21 +20,23 @@ function HomeContainer() {
       <h1>. . .</h1>
       <Parallax bgImage={image1} strength={500} blur={{ min: -1, max: 3 }}>
         <div style={{ height: 800 }}>
-          <div style={inlineStyle}>Our Story</div>
+          <div style={inlineStyle}>Our Story<br/>Before opening his first glorious leader and founder V.G Childe had invented in the British cheese industry.<br/> For some time he had felt that there was a lack of opportunity to buy high quality farmhouse cheese, both in the retail and wholesale sector.
+           <br/> Childe Cheesemongers have now been trading for over 150 years, bringing together producers and consumers for mutual benefit. The aim for the future is to continue this process while continually striving to improve
+            </div>
         </div>
       </Parallax>
 
       <h1>. . .</h1>
       <Parallax bgImage={image2} blur={{ min: -1, max: 3 }}>
         <div style={{ height: 800 }}>
-          <p style={inlineStyle}>Our Vision</p>
+          <p style={inlineStyle}>Our Vision<br/>Is to bring you the best produce from Scotland and the world.</p>
         </div>
       </Parallax>
 
       <h1>. . .</h1>
       <Parallax bgImage={image3} strength={-200}>
         <div style={{ height: 800 }}>
-          <div style={inlineStyle}>Our Promise</div>
+          <div style={inlineStyle}>Our Promise<br/> We only use ethical and local suppliers to bring you the highest quality produce</div>
         </div>
       </Parallax>
       <h1>.  .  .</h1>

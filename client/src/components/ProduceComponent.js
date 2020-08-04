52ef924691814429ae99da1b363ae2aa00ccfa21
Jack Portwood <Jack.Portwood14@gmail.com>
2020-08-04 12:58:40 +0100

    styling added to produce container

--- client/src/components/ProduceComponent.js
+++ client/src/components/ProduceComponent.js
@@ -2,10 +2,7 @@ import React, { Fragment } from "react";
 import { NavLink,  } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheese } from '@fortawesome/free-solid-svg-icons'
-// import image4 from "../images/image4.jpg";
-// import image5 from "../images/image5.jpg";
-// import image6 from "../images/image6.jpg";
-// import image7 from "../images/image7.jpg";
+
 
 function ProduceContainer() {
   return (
@@ -15,22 +12,18 @@ function ProduceContainer() {
       <div className="produce-boxes">
         <NavLink to="/bluecheesecomp" className="produce-link">
           Blue Cheese
-          {/* <img src={image4} height="300" width="300" alt="cheesePic"></img> */}
         </NavLink>
 
         <NavLink to="/hardcheesecomp" className="produce-link">
           Hard Cheese
-          {/* <img src={image5} height="300" width="300" alt="cheesePic"></img> */}
         </NavLink>
 
         <NavLink to="/softcheesecomp" className="produce-link">
           Soft Cheese
-          {/* <img src={image6} height="300" width="300" alt="cheesePic"></img> */}
         </NavLink>
 
         <NavLink to="/pantrycomp" className="produce-link">
           Pantry Items
-          {/* Pantry<img src={image7} height="300" width="300" alt="cheesePic"></img> */}
         </NavLink>
       </div>
     </Fragment>
